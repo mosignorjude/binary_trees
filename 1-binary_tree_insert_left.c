36754749db9c0bd134001f4936badcc94414046c
mosignorjude <mosignorjude2@gmail.com>
2024-06-05 00:33:21 +0100

    fixed insert left function errors

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,10 +15,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	left_child_node = malloc(sizeof(binary_tree_t));
+	left_child_node = binary_tree_node(parent, value);
 	if (left_child_node == NULL)
 		return (NULL);
-	left_child_node = binary_tree_node(parent, value);
 	if (parent->left != NULL)
 	{
 		left_child_node->left = parent->left;
