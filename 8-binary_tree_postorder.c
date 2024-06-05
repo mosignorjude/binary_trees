a56aeefbf80985c7abf7a6caba1290552af1deab
mosignorjude <mosignorjude2@gmail.com>
2024-06-05 02:22:28 +0100

    fixed betty error in post order traversal file

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,7 +2,8 @@
 #include <linux/limits.h>
 
 /**
- * binary_tree_postorder -  goes through a binary tree using post-order traversal
+ * binary_tree_postorder -  goes through a binary tree using post-order
+ * traversal
  * @tree:  pointer to the root node of the tree to traverse.
  * @func:  pointer to a function to call for each node.
  *
