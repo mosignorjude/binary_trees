2086354351e1cd1f9a3e568825fcc02d0a6c450a
mosignorjude <mosignorjude2@gmail.com>
2024-06-06 13:02:08 +0100

    implemented the function to calculate the size of a binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-#include <stddef.h>
 
 /**
  * binary_tree_depth - measures the depth of a node in a binary tree.
@@ -13,5 +12,5 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL || tree->parent == NULL)
 		return (0);
 
-	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	return (1 + binary_tree_depth(tree->parent));
 }
