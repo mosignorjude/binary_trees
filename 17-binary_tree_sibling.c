00de60b8a092e14a35c74d4e14363726d126468a
mosignorjude <mosignorjude2@gmail.com>
2024-06-07 00:03:35 +0100

    fix error in binary tree sibling function

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,9 +11,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node->parent == NULL || node == NULL)
 		return (NULL);
 
-	if (node == node->parent->right && node->parent->left)
+	if (node == node->parent->right)
 		return (node->parent->left);
-	else if (node == node->parent->left && node->parent->right)
+	else if (node == node->parent->left)
 		return (node->parent->right);
 
 	return (NULL);
