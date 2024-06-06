ad2a25cbbf279e7f65c95903de3f78efd7120de9
mosignorjude <mosignorjude2@gmail.com>
2024-06-07 00:00:46 +0100

    fixed error in binary tree uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,14 +8,14 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node->parent->parent == NULL || node->parent == NULL ||
-			node == NULL)
-		return (NULL);
+	binary_tree_t *grand_parent;
 
-	if (node == node->parent->right && node->parent->parent->right)
-		return (node->parent->parent->right);
-	else if (node == node->parent->left && node->parent->parent->left)
-		return (node->parent->parent->left);
-	else
+	if (node == NULL || node->parent == NULL ||
+			node->parent->parent == NULL)
 		return (NULL);
+	grand_parent = node->parent->parent;
+	if (node->parent == grand_parent->right)
+		return (grand_parent->left);
+	else
+		return (grand_parent->left);
 }
