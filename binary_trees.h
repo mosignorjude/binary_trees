438bcf22880b05c88d436b8252b9ed92c7096a26
mosignorjude <mosignorjude2@gmail.com>
2024-06-07 01:07:11 +0100

    rotate, ancestor, levelorder, is_complete functions

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,17 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
+/**
+ * struct levelorder_queue_s - Level order traversal queue.
+ * @node: A node of a binary tree.
+ * @next: The next node to traverse to in the binary tree.
+ */
+typedef struct levelorder_queue_s
+{
+	binary_tree_t *node;
+	struct levelorder_queue_s *next;
+} levelorder_queue_t;
+
 /*Binary Search Tree*/
 typedef struct binary_tree_s bst_t;
 /*AVL Tree*/
@@ -30,6 +41,7 @@ typedef struct binary_tree_s avl_t;
 /*Max Binary Heap*/
 typedef struct binary_tree_s heap_t;
 
+/* Task function prototypes */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
